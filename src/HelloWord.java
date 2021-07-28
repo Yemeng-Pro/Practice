1f1ad9c846b10110dd5a22e734ab7f3ccbdea38c
gongziwei1 <gongziwei1@jd.com>
2021-07-28 16:35:24 +0800

    Initial commit

---
+++ src/HelloWord.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class HelloWord {
+    public static void main(String[] args) {
+        String a = "";
+        System.out.println(a.length());
+    }
+
+}
---
+++ src/ThreadPool/ThreadDemo.java
@@ -0,0 +1,26 @@
+package ThreadPool;
+
+/**
+ * @author gongziwei1@jd.com  7月 28,2021
+ * @description:
+ * @Version 1.0.0
+ * @modify:
+ * @Copyright:jd.com
+ */
+public class ThreadDemo {
+    public static void main(String[] args) {
+
+    }
+
+}
+
+
+class ThreadPool{
+
+}
+
+
+class BlockingQue<T>{
+
+
+}
